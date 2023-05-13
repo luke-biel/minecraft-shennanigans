484bba5441be62dcaad1e1e7075c67de133cfb41
Łukasz Biel <lukasz.p.biel@gmail.com>
2023-05-13 17:43:53 +0200

    WIP

--- farmer.lua
+++ farmer.lua
@@ -1,7 +1,6 @@
 WHEAT_MATURE_AGE=7
 
 --[[
-# Pick up fuel
     c 
   c<t o o o o o o o 
     o o o o o o o o 
@@ -12,9 +11,52 @@ WHEAT_MATURE_AGE=7
     o o o o o o o o 
     o o o o o o o o 
 ]] 
-turtle.suck(64) 
-turtle.refuel()
+function refuel()
+  turtle.suck(8) 
+  turtle.refuel()
+end
 
+--[[
+  This method finds seeds in the inventory, moves them to slot one and selects that slot
+]]
+function moveSeedsToSlot1() 
+  -- Find slot with seeds
+  seeds = -1
+  for i=0,15,1 do
+    detail = turtle.getItemDetail(i)
+    if detail.name == "minecraft:wheat_seeds" then
+      seeds = i
+    end
+  end
+
+  if seeds >= 0 then
+    -- Move seeds to slot one
+    turtle.select(seeds)
+    turtle.transforTo(1)
+  else
+    -- Empty out the slot 1, so no item placement happens
+    print("Error, No seeds")
+
+    empty = -1
+    for i=0,15,1 do
+      detail = turtle.getItemDetail(i)
+      if detail ~= nil then empty = i end
+    end
+    if empty >= 0 then
+      turtle.select(1)
+      turtle.transforTo(empty)
+    else
+      print("Error, Inventory full!")
+    end
+  end
+
+  -- Select slot one
+  turtle.select(1)
+end
+
+--[[
+  Check if the block is harvestable wheat, harvest it, find seeds in inventory, and plant them
+]]
 function harvest()
   isBlock, block = turtle.inspectDown()
   if not isBlock then return end
@@ -22,13 +64,31 @@ function harvest()
   if not block.state.age == WHEAT_MATURE_AGE then return end
   -- block is a mature wheat
   turtle.digDown()
+  moveSeedsToSlot1()
+  turtle.placeDown()
+end
+
+--[[
+  Drops off wheat into inventory in front
+]]
+function dropOffWheat() 
+  for i=0,15,1 do
+    detail = turtle.getItemDetail(i)
+    if detail.name == "minecraft:wheat_seeds" || detail.name == "minecraft:wheat" then
+      turtle.select(i)
+      turtle.drop()
+    end
+  end
+  turtle.select(1)
 end
 
 --[[
 # Start moving and harvesting
 ]]
 turtle.turnLeft()
+-- Repeat 4 times
 for i=0,3,1 do
+  -- Move to the end in first row
   for j=0,6,1 do
     harvest()
     turtle.forward()
@@ -37,6 +97,7 @@ for i=0,3,1 do
   turtle.forward()
   turtle.turnLeft()
 
+  -- Move to the end in second row
   for j=0,6,1 do
     harvest()
     turtle.forward()
@@ -49,7 +110,11 @@ end
 --[[
   return to base station
 ]]
-turtle.turnLeft()
+turtle.turnRight()
 for i = 0,7,1 do
   turtle.forward()
 end
+
+turtle.turnRight()
+dropOffWheat()
+turtle.turnLeft()
