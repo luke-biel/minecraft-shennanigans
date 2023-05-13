49008e7f790d833ac9360017f68a963063aedaf2
Łukasz Biel <lukasz.p.biel@gmail.com>
2023-05-13 16:55:32 +0200

    wIP

---
+++ farmer.lua
@@ -0,0 +1,47 @@
+WHEAT_MATURE_AGE=7
+
+--[[
+# Pick up fuel
+    c 
+  c<t o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+    o o o o o o o o 
+]] 
+turtle.suck(64) 
+turtle.refuel()
+
+function harvest()
+  isBlock, block = turtle.inspectDown()
+  if not isBlock then return end
+  if not block.name == "minecraft:wheat" then return end
+  if not block.state.age == WHEAT_MATURE_AGE then return end
+  -- block is a mature wheat
+  turtle.digDown()
+end
+
+--[[
+# Start moving and harvesting
+]]
+turtle.turnLeft()
+for i=0,7,1 do
+  for j=0,7,1 do
+    harvest()
+    turtle.forward()
+  end
+  turtle.turnLeft()
+  turtle.forward()
+  turtle.turnLeft()
+end
+
+--[[
+  return to base station
+]]
+turtle.turnLeft()
+for i = 0,7,1 do
+  turtle.forward()
+end
